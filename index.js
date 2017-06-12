fa6406884757bea0884318c97b2e2e4b99d69d89
chang_su <chang_su@hunliji.com>
2017-06-12 11:20:58 +0800

    node index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by chang_su on 2017/6/12.
+ */
+const Koa = require('koa')
+const app = new Koa()
+
+app.use( async ( ctx ) => {
+    ctx.body = 'hello koa2'
+})
+
+app.listen(3000)
+console.log('[demo] start-quick is starting at port 3000')
